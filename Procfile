740912bdf1e16928bad7274a8656314314752afb
fzapfack <fabricezapfack@gmail.com>
2017-02-01 11:02:28 +0100

    celery add max connections

--- Procfile
+++ Procfile
@@ -1,3 +1,2 @@
 web: gunicorn gettingstarted.wsgi --log-file -
 worker: celery worker --app=gettingstarted.worker.app
-# worker: python worker2.py
--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -46,7 +46,6 @@ INSTALLED_APPS = (
     'hello',
     'listener',
     'predictor',
-    # 'multiselectfield',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -157,8 +156,14 @@ DEFAULT_INDEX_TABLESPACE = ''
 # CELERY STUFF
 # BROKER_URL = 'redis://localhost:6379'
 # CELERY_RESULT_BACKEND = 'redis://localhost:6379'
-BROKER_URL = 'redis://h:p11b5d16752910cb350d36ec56be298f158bb0be519ffa64918ac786130fecf8d@ec2-34-198-124-158.compute-1.amazonaws.com:14219'
-CELERY_RESULT_BACKEND = 'redis://h:p11b5d16752910cb350d36ec56be298f158bb0be519ffa64918ac786130fecf8d@ec2-34-198-124-158.compute-1.amazonaws.com:14219'
+BROKER_URL = os.environ.get('REDIS_URL',
+                            'redis://h:p11b5d16752910cb350d36ec56be298f158bb0be519ffa64918ac786130fecf8d@ec2-34-198-124-158.compute-1.amazonaws.com:14219')
+# CELERY_RESULT_BACKEND = 'redis://h:p11b5d16752910cb350d36ec56be298f158bb0be519ffa64918ac786130fecf8d@ec2-34-198-124-158.compute-1.amazonaws.com:14219'
+# BROKER_URL = os.environ.get("REDISCLOUD_URL", "django://")
+BROKER_TRANSPORT_OPTIONS = {
+    "max_connections": 2,
+}
+# BROKER_POOL_LIMIT = None
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
