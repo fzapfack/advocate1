4d6bff9a859eeb432ef6bec1ee2a9003fdff0366
fzapfack <fabricezapfack@gmail.com>
2017-02-15 11:44:16 +0100

    add tittle to map

--- hello/views.py
+++ hello/views.py
@@ -1,24 +1,11 @@
 from django.shortcuts import render
 import json
+from django.db.models import Q
 from .models import Greeting
 from listener.models import Tweet as TweetModel
 from hello.models import Region
 from hello.utils.carte_fr import Map
 
-from hello.models import Region
-regions = Region.objects.all()
-for r in regions:
-    r.num_tweets_pos = 0
-    r.num_tweets_neg = 0
-    r.num_tweets_net = 0
-    r.save()
-
-from listener.models import Tweet as TweetModel
-res = TweetModel.objects.all()
-for t in res:
-    t.added_map = False
-    t.usr_region = None
-    t.save()
 
 # Create your views here.
 def db(request):
@@ -68,9 +55,9 @@ def index(request):
                 "name": r.name,
                 "color": r.color,
                 "position": {"lng": float(sum(pos[::2]))/len(pos[::2]), "lat": float(sum(pos[1::2]))/len(pos[1::2])},
-                "num_pos": r.num_tweets_pos,
-                "num_neg": r.num_tweets_neg,
-                "num_net": r.num_tweets_net,
+                "reg_num_pos": r.num_tweets_pos,
+                "reg_num_neg": r.num_tweets_neg,
+                "reg_num_net": r.num_tweets_net,
                 "num_tweets": r.num_tweets_pos + r.num_tweets_neg + r.num_tweets_net
             },
             "geometry": {
@@ -94,6 +81,7 @@ def index(request):
     context['num_neg'] = TweetModel.objects.filter(sentiment_label=TweetModel.SENTIMENTS['NEGATIVE']).count()
     context['num_net'] = TweetModel.objects.filter(sentiment_label=TweetModel.SENTIMENTS['NEUTRAL']).count()
     context['num_net'] = TweetModel.objects.filter(sentiment_label=TweetModel.SENTIMENTS['NEUTRAL']).count()
+    context['num_local'] = TweetModel.objects.filter(~Q(sentiment_label=None) & ~Q(usr_region='UNKNOWN')).count()
     context['geojson'] = json.dumps(geojson)
 
     return render(request, 'index.html', context)
