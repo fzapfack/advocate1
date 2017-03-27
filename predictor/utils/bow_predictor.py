7d08ab66d00823d2c1a01c6f0493eacfdc947c2b
fzapfack <fabricezapfack@gmail.com>
2017-03-28 00:21:19 +0200

    pipeline

--- predictor/utils/bow_predictor.py
+++ predictor/utils/bow_predictor.py
@@ -2,9 +2,15 @@ import nltk
 import re
 import string
 from unidecode import unidecode
+import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn import preprocessing
+from sklearn.decomposition import TruncatedSVD
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
 from django.db.models import Q
 from django.db.models import F
 from listener.models import Tweet
@@ -13,17 +19,10 @@ from hello.utils.carte_fr import Map
 
 nltk.data.path.append('predictor/data/nltk')
 
-class BowPredictor:
-    predictor = None
 
-    def __init__(self):
-        self.stpwds = nltk.corpus.stopwords.words("french")
-        self.vectorizer = None
-        self.tfidf = None
-        self.clf = None
-        self.train_id = None
-        self.test_id = None
-
+class TweetTokenizer(BaseEstimator, TransformerMixin):
+    def __init__(self, rep_sub=True):
+        self.rep_sub = rep_sub
 
     def tokenize(self, content):
         if content is None:
@@ -59,51 +58,118 @@ class BowPredictor:
             tokens = content.split(" ")
             # remove stopwords
             stpwds = nltk.corpus.stopwords.words("french")
+            others = ['le', 'les', 'la', 'etait', 'cette']
+            _ = [stpwds.append(i) for i in others if i not in stpwds]
             tokens = [token for token in tokens if token not in stpwds and len(token) > 2]
             return tokens
 
-    # def replace_subject(txt, sign='XXXX'):
-    #     clean = txt
-    #     clean = re.sub('[#@][mM][aA][cC][rR][oO][nN]\w*', sign, clean)
-    #     clean = re.sub('[mM][aA][cC][rR][oO][nN]\w*', sign, clean)
-    #     return clean
+    def replace_subject(self, txt, sign='XXXX'):
+        clean = txt
+        clean = re.sub('[#@][mM][aA][cC][rR][oO][nN]\w*', sign, clean)
+        clean = re.sub('[mM][aA][cC][rR][oO][nN]\w*', sign, clean)
+        return clean
 
-    # def tweet_to_token(self, tweet, replace_subject=True):
-    #     txt = tweet.txt
-    #     if replace_subject:
-    #         txt = self.replace_subject(txt, sign='XXXX')
-    #     tokens = self.tokenize(txt)
-    #     return tokens
+    def fit(self, X, y=None):
+        pass
+        return self
+
+    def transform(self, X, y=None):
+        tokens = []
+        l = X
+        if isinstance(l, str):
+            l = [l]
+        for txt in l:
+            #             txt = d.txt
+            if self.rep_sub:
+                txt = self.replace_subject(txt)
+            tokens.append(self.tokenize(txt))
+        join_tokens = [" ".join(i) for i in tokens]
+        return join_tokens
+
+
+class Preprocessor(BaseEstimator, TransformerMixin):
+
+    def __init__(self, normalize=False, dim_reduc=None, k=0.5):
+        self.normalize = normalize
+        self.dim_reduc = dim_reduc
+        self.k = k
+
+    def transform(self, X):
+        if self.normalize:
+            X = self.scaler.transform(X)
+        if self.dim_reduc=='svd':
+            X = self.svd.transform(X)
+        elif self.dim_reduc=='lsi':
+            pass
+
+        return X.toarray()
+
+    def fit(self, X, y=None):
+        if self.normalize:
+            # Normalization
+            self.scaler = preprocessing.StandardScaler().fit(X)
+        if self.dim_reduc=='svd':
+            n_components = round(X.shape[1]*self.k)
+            self.svd = TruncatedSVD(n_components=n_components).fit(X)
+        if self.dim_reduc=='lsi':
+            pass
+        return self
+
+
+class BowPredictor:
+    predictor = None
+
+    def __init__(self):
+        self.stpwds = nltk.corpus.stopwords.words("french")
+        self.vectorizer = None
+        self.tfidf = None
+        self.clf = None
+        self.train_id = None
+        self.test_id = None
 
     def train(self, update_predictor=True):
-        labelled = Tweet.objects.filter(~Q(sentiment_label=None))
+        labelled = Tweet.objects.filter(~Q(sentiment_label=None) & Q(lang='fr') & ~Q(sentiment_label=2))
         tokens = []
         y_train = []
         for l in labelled:
-            tokens.append(self.tokenize(l.txt))
+            tokens.append(l.txt)
             y_train.append(l.sentiment_label)
             l.training_tweet = True
             l.save()
 
-        voc = list(set([i for sublist in tokens if sublist is not None for i in sublist]))
-        self.vectorizer = CountVectorizer(vocabulary=voc, ngram_range=(1,2))
-        join_tokens = [" ".join(i) for i in tokens]
-        dtm = self.vectorizer.fit_transform(join_tokens)
-        self.tfidf = TfidfTransformer(use_idf=True, smooth_idf=True).fit(dtm)
-        X_train = self.tfidf.transform(dtm)
-        self.clf = LogisticRegression(class_weight='balanced', fit_intercept=False, C=10).fit(X_train, y_train)
+        self.clf = Pipeline([('tokenizer', TweetTokenizer()),
+                         ('vect', CountVectorizer()),
+                         ('tfidf', TfidfTransformer()),
+                         ('preproc', Preprocessor()),
+                         ('clf', LogisticRegression(solver='lbfgs',
+                                                    multi_class='multinomial'))
+                         ])
+        params = {'clf__C': 10,
+                  'clf__class_weight': 'balanced',
+                  'clf__fit_intercept': False,
+                  'tfidf__smooth_idf': False,
+                  'tfidf__use_idf': True,
+                  'tokenizer__rep_sub': False,
+                  'vect__ngram_range': (1, 2)
+                  }
+        self.clf.set_params(**params)
+        self.clf.fit(tokens, y_train)
+
+        print("\n\n >>>>results training dataset")
+        y_pred = self.clf.predict(tokens)
+        labels = list(set(y_train))
+        print(classification_report(y_true=y_train, y_pred=y_pred, labels=labels,
+                                    target_names=['neg', 'net', 'pos']))
+        print("Accuracy: ", np.sum(np.array(y_train) == np.array(y_pred)) / len(np.array(y_train)))
         if update_predictor:
             BowPredictor.predictor = self
         return True
 
     def predict_tweet(self, tweet):
-        tokens = self.tokenize(tweet.txt)
-        join_tokens = " ".join(tokens)
-        vect = self.vectorizer.transform([join_tokens])
-        x_pred = self.tfidf.transform(vect)
-        pred = self.clf.predict(x_pred)
-        pred = pred[0]
-        return pred
+        y_pred = self.clf.predict(tweet.txt)
+        if ~isinstance(y_pred, int):
+            y_pred = int(y_pred[0])
+        return y_pred
 
     @staticmethod
     def predict_tweet_static(tweet, predictor=None, save=True):
